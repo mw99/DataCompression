4e2d91e11f2e8eda622aa8032555ac23401dba7c
Cihat Gündüz <commits@cihatguenduez.de>
2018-12-15 11:07:49 +0100

    Create Xcode project with shared framework schemes for Carthage support

---
+++ CarthageSupport/DataCompression/SupportingFiles/DataCompression.h
@@ -0,0 +1,19 @@
+//
+//  DataCompression.h
+//  DataCompression
+//
+//  Created by Cihat Gündüz on 15.12.18.
+//  Copyright © 2018 Markus Wanke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataCompression.
+FOUNDATION_EXPORT double DataCompressionVersionNumber;
+
+//! Project version string for DataCompression.
+FOUNDATION_EXPORT const unsigned char DataCompressionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataCompression/PublicHeader.h>
+
+
